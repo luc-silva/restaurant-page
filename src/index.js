ace71008070f3a971dab5b9b0ad0c0a480404c34
Lucas Santos <lucas.silva1203@outlook.com>
2022-11-15 19:28:37 -0300

    Add homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './style.css'
+
+import initializeHomepage from './modules/home';
+import './style.css';
+
+
+let homeBtn = document.querySelector("#home-btn");
+homeBtn.addEventListener("click", initializeHomepage);
+
---
+++ src/modules/home.js
@@ -0,0 +1,29 @@
+function createHomepage(){
+    let page = document.createElement("section")
+    page.id = "home"
+
+    let div = document.createElement("div")
+    div.innerHTML = `
+    <div id="home-welcome">
+    <h2>Welcome to the Low Carbs Clinic!</h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ipsum fuga quia cum quaerat, exercitationem, unde tempore doloribus nemo provident quibusdam pariatur velit perspiciatis assumenda eum, iusto ut quae totam!
+    </div>`
+    div.id = "home-background"
+
+    page.appendChild(div)
+    return page
+
+}
+
+function initializeHomepage(){
+    let body = document.querySelector("#content")
+    
+    let homepage = createHomepage()
+
+    alert(homepage)
+
+    body.appendChild(homepage)
+
+}
+
+export default initializeHomepage;
