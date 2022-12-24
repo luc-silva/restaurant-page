302152d6db8a48c231a85bcc4b948b0dab87e4c8
Lucas Santos <lucas.silva1203@outlook.com>
2022-12-24 19:00:47 -0300

    Add babel to the project

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,16 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'public/main.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
