05bc05f51fb3f6ecaa6334295014c18b727eeaec
Lucas Santos <lucas.silva1203@outlook.com>
2022-12-24 18:38:38 -0300

    Remove files from the old version

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import initializeHomepage from './modules/home.js'
-import initializeMenu from './modules/menu.js'
-import initializeAboutUs from './modules/about-us.js'
-import initializeDelivery from './modules/delivery.js'
+import React, { Component } from "react"
 
-let header = (function(){
-    let homeBtn = document.querySelector("#home-btn")
-    homeBtn.addEventListener("click", initializeHomepage)
-
-    let menuBtn = document.querySelector("#menu-btn")
-    menuBtn.addEventListener("click", initializeMenu)
-
-    let aboutBtn = document.querySelector("#about-btn")
-    aboutBtn.addEventListener("click", initializeAboutUs)
-
-    let deliveryBtn = document.querySelector("#delivery-btn")
-    deliveryBtn.addEventListener("click", initializeDelivery)
-
-
-})()
-
-initializeHomepage()
+class Restaurant extends Component{
+    
+}
