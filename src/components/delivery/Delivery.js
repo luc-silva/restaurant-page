b929b191e47c0818520d6ebba2d2f0072d902c53
Lucas Santos <lucas.silva1203@outlook.com>
2023-01-10 20:34:34 -0300

    Add delivery page component

---
+++ src/components/delivery/Delivery.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import { DeliveryForm } from "./DeliveryForm";
+
+export const Delivery = () => {
+    return (
+        <section id="delivery-section">
+            <div id="section-picture"></div>
+            <DeliveryForm />
+        </section>
+    );
+};
+
+
---
+++ src/components/delivery/DeliveryForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export const DeliveryForm = () => {
+    return (
+        <div id="delivery-container">
+            <div id="delivery-input">
+                <input type="text" placeholder="Your name" />
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Address" />
+                <button>Request free low carb suply!</button>
+            </div>
+            <div id="delivery-text">
+                <h2>Request free stuff right now!</h2>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Necessitatibus nulla aspernatur, quasi, temporibus assumenda
+                    accusamus magnam placeat pariatur, nihil eius veritatis
+                    adipisci. Sit sint est inventore culpa nostrum quidem
+                    sapiente!
+                </p>
+            </div>
+        </div>
+    );
+};
