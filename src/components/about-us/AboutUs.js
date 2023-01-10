6c0191270a94ac726dbfcf5baf15a206aae61e69
Lucas Santos <lucas.silva1203@outlook.com>
2023-01-10 20:34:20 -0300

    Add about us page component

---
+++ src/components/about-us/AboutUs.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import AboutUsContainer from "./AboutUsContainer";
+
+export const AboutUs = () => {
+    return (
+        <section id="about-us">
+            <AboutUsContainer />
+            <div id="about-us-img"></div>
+        </section>
+    );
+};
---
+++ src/components/about-us/AboutUsContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+const AboutUsContainer = () => {
+    return (
+        <div id="about-us-container">
+            <div id="about-us-info">
+                <h2>About Us</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptas sunt aperiam quam tempora, ex repellendus odit
+                    alias consequatur voluptates, assumenda illo? Voluptas quo
+                    minima unde earum pariatur fuga necessitatibus amet?
+                </p>
+            </div>
+
+            <div id="about-us-location-list">
+                <h3>You can find us at:</h3>
+                <ul>
+                    <li>1232 Oysterdale Rd - Pennsylvania</li>
+                    <li>1641 N State St - Utah</li>
+                    <li>5100 Curry Hwy - Alaska</li>
+                    <li>800 Greenwood Ave - North Dakota</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default AboutUsContainer;
